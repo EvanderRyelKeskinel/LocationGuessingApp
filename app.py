f3c086f0d4b940b6269fe1778447165f3befe674
EvanderRyelKeskinel <157602579+EvanderRyelKeskinel@users.noreply.github.com>
2024-12-26 16:56:42 +0000

    sent latlong to solo

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 import json
 import sqlite3
-
+import geopy
 import requests
+import math
 from flask import Flask, redirect, render_template, request, session, url_for
+from geopy.distance import geodesic
+
 
 app = Flask(__name__)
+app.secret_key = 'abc123'
+
+def distance(lat1, lng1, lat2, lng2):
+	d1 = geodesic((lat1, lng1), (lat2, lng2))
+	d = (float(d1.km))
+	""" Convert geodesic datatype to float"""
+	return d
+
+
+def points(d):
+	points = 10000 * (math.exp(-1 * (d / 1000)) - math.exp(-1 * (20038 / 1000)))
+	return points
 
 def create():
 	with sqlite3.connect('login.db') as db:
@@ -38,19 +53,15 @@ def login():
 
 @app.route('/latlong')
 def latlong():
-	"""global lat
-	global lng """
 	lat = request.args.get('lat')
 	lng = request.args.get('lng')
-	print(lat, lng)
-	return lat
-def getlatlong():
-	global latget
-	global lngget
-	latget = session.get('latget', None)
-	lngget = session.get('lngget', None)
-	print(latget)
-	return latget
+	if lat and lng: """runs if we have values for lat and lng"""
+	session['lat'] = lat
+	session['lng'] = lng
+	
+	return redirect(url_for('solo'))
+
+
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -78,23 +89,24 @@ def solo():
 	bbox = "-180,-90,180,90" 
 	x = requests.get(base + bbox, params={'limit': 10})
 	parsed_data = json.loads(x.text)
+	print(parsed_data)
 	image = parsed_data['data'][0]['id']
-	access_token = "MLY|7884436731651628|991d31489dc0ba2a68fd9c321c4d2cd1"
 	print(image)
+	access_token = "MLY|7884436731651628|991d31489dc0ba2a68fd9c321c4d2cd1"
 	url = f"https://graph.mapillary.com/{image}?access_token={access_token}&fields=id,computed_geometry,detections.value"
 	y = requests.get(url, params={'limit': 1})
 	locations = json.loads(y.text)
-	print(locations)
 	lngget = locations['computed_geometry']['coordinates'][0]
 	latget = locations['computed_geometry']['coordinates'][1]
-	def sendlatlng():
-		session['lngget'] = lngget
-		session['latget'] = latget
-		return redirect(url_for('latlong'))
-	
+	lat = session.get('lat', None)
+	lng = session.get('lng', None)
+	print(lat)
+	dist = distance(latget, lngget, lat, lng)
+	print(dist)
+	point = points(dist)
 	
 	
 	return render_template('solo.html', image=image)
-
+	
 if __name__ == "__main__":
     app.run(debug=True)
